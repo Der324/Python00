458e52099950cf70e57844fd918a757b70d0a81d
Der324 <ntalenderick@gmail.com>
2024-08-23 19:57:20 +0300

    My initial project with python.

---
+++ app python.py
@@ -0,0 +1,9 @@
+character_name = "Danny"
+character_age = "100"
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+
+character_name = "Tina"
+print("He really liked the name " +character_name + ", ")
+print("but didn't like being " + character_age + ". ")
