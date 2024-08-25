4915853b3315a4afc12fdfa0e4ca6fcdf63f8f4e
Der324 <ntalenderick@gmail.com>
2024-08-25 23:48:08 +0300

    Numbers with python!

---
+++ Numbers python.py
@@ -0,0 +1,29 @@
+print(24-26)
+print(6-(2*6))
+
+my_num = 35
+print(my_num)
+
+my_num = 11
+print(str(my_num))
+
+my_num = 28
+print(str(my_num) + " is my awesome digits")
+
+my_num = -11
+print(abs(my_num))
+
+print(pow(2, 1))
+
+my_num = 3
+print(pow(my_num, 2))
+
+print(max(4,3))
+print(min(3,5))
+print(round(3.14))
+
+from math import*
+
+print(floor(2.8))
+print(ceil(2.8))
+print(sqrt(64))
