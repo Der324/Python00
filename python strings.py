42ca857d139c77dcf8c430f84cd3be9d1b8ac523
Der324 <ntalenderick@gmail.com>
2024-08-25 19:37:28 +0300

    working with strings!

---
+++ python strings.py
@@ -0,0 +1,26 @@
+print("Happy\nAfrican")
+
+print("Happy\"African")
+
+phrase = "Happy African"
+print(phrase + " is awesome")
+
+phrase = "Happy African"
+print(phrase.lower())
+print(phrase.lower().islower())
+
+phrase = "happy african"
+print(phrase.upper())
+print(phrase.upper().isupper())
+
+phrase = "Happy African"
+print(len(phrase))
+
+phrase = "Happy African"
+print(phrase[10])
+
+phrase = "New World Order"
+print(phrase.index("W"))
+
+phrase = "New World Order"
+print(phrase.replace("World" , "African"))
