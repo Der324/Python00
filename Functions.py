e91af680dfc531833d82c1307af444e5ad9aadd2
Der324 <ntalenderick@gmail.com>
2024-08-27 01:41:06 +0300

    Basic Functions!

---
+++ Functions.py
@@ -0,0 +1,24 @@
+
+def sayhelloworld():
+    print("Earth is awesome!")
+
+sayhelloworld()
+
+def say_helloworld(name):
+    print("Earth is awesome " + name)
+
+say_helloworld("Mike!")
+say_helloworld("Steve!")
+
+def say_hello(izina, imyaka):
+    print("Amakuru " + izina + ", ufite " + imyaka)
+
+say_hello("mike", "45 years")
+say_hello("Steve", "100 years")
+
+def say_hi(name, age):
+    print("Hello " + name + ", you are " + str(age))
+
+say_hi("mike", 60 )
+say_hi("Steve", 18)
+
