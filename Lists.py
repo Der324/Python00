b9b29e33cf977cda9824a77c856cd97761dcc361
Der324 <ntalenderick@gmail.com>
2024-08-26 13:50:41 +0300

    Basic Lists!

---
+++ Lists.py
@@ -0,0 +1,19 @@
+
+friends = ["Nkotanyi", "Ntale", "Derick"]
+print(friends)
+
+friends = ["Nkotanyi", "Ntale", "Derick"]
+print(friends[0])
+
+friends = ["Nkotanyi", "Ntale", "Derick"]
+print(friends[-1])
+
+friends = ["Nkotanyi", "Ntale", "Derick", "Rugamba", "Ntwali"]
+print(friends[1:])
+
+friends = ["Nkotanyi", "Ntale", "Derick", "Rugamba", "Ntwali"]
+print(friends[1:4])
+
+friends = ["Nkotanyi", "Ntale", "Derick", "Rugamba", "Ntwali"]
+friends[2] = "Keisha"
+print(friends[2])
