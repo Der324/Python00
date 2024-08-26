12ef6d89c3433a4ff8edc8e0a96d5dab2929891f
Der324 <ntalenderick@gmail.com>
2024-08-26 12:08:21 +0300

    Mad libs games!

---
+++ Mad libs game.py
@@ -0,0 +1,8 @@
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural_noun: ")
+famous_city = input("Enter a Famous_city: ")
+
+print("Roses are " + color)
+print(plural_noun  +  " are blue ")
+print("I love " + famous_city)
