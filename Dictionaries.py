5cbe697a98dea69affb1c59c51e628cfaa769d99
Der324 <ntalenderick@gmail.com>
2024-08-27 13:13:16 +0300

    build a Dictionary

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+
+
+monthconversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+        4: "April",
+        5: "May",
+        6: "June",
+}
+
+print(monthconversions[4])
+print(monthconversions.get(5))
