bd217575b2a15d221b744418bbef4e38535bd7ab
Der324 <ntalenderick@gmail.com>
2024-08-27 15:28:12 +0300

    Using for loop!

---
+++ For loop.py
@@ -0,0 +1,22 @@
+
+for letter in "Happy Life":
+    print(letter)
+
+friends = ["ntale", "derick", "nick"]
+for friend in friends:
+    print(friend)
+
+for numbers in range(0,15):
+    print(numbers)
+
+friends = ["ntale", "derick", "nick"]
+for names in range(len(friends)):
+    print(friends[names])
+
+
+
+for index in range(3):
+    if index == 2:
+        print("first Iteration")
+    else:
+        print("Not first")
