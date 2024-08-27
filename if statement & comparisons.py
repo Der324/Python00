4d189dfa34e3bccdbc7220921efe667b89616ace
Der324 <ntalenderick@gmail.com>
2024-08-27 10:02:14 +0300

    if statement & comparisons!

---
+++ if statement & comparisons.py
@@ -0,0 +1,12 @@
+
+def max_num(num1, num2, num3, num4):
+    if num1 <= num2 and num1 <= num3 and num1 <= num4:
+        return num1
+    elif num2 <= num1 and num2 <= num3 and num2 <= num4:
+        return num2
+    elif num3 <= num1 and num3 <= num2 and num3 <= num4:
+        return num3
+    else:
+        return num4
+
+print(max_num(75, 9, 20, 100))
