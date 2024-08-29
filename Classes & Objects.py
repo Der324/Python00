68ae5fc697ec064717b20c0d26d32e27a0a04ddf
Der324 <ntalenderick@gmail.com>
2024-08-29 14:55:24 +0300

    Creating objects from classes!

---
+++ Classes & Objects.py
@@ -0,0 +1,9 @@
+
+from Student import Student
+
+student01 = Student("Usher", "Engineering", 3.1, False)
+student02 = Student("Bwiza", "Art", 4.1, True)
+
+
+print(student01.major)
+print(student02.gpa)
